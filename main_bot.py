a5cde925cd63c0d8f40355dc980dd2e9c691f056
alexeev509 <alexeevy509@gmail.com>
2022-11-15 18:46:30 +0300

    Add selenium

--- main_bot.py
+++ main_bot.py
@@ -8,6 +8,9 @@ TOKEN = os.environ.get('TOKEN')
 bot = telebot.TeleBot(TOKEN)
 
 chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
