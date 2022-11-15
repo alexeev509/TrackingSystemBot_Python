fe615ec03b99d22c31c222507db74848a5cf546c
alexeev509 <alexeevy509@gmail.com>
2022-11-15 18:08:54 +0300

    Add selenium

--- main_bot.py
+++ main_bot.py
@@ -1,9 +1,15 @@
 import telebot
 import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 TOKEN = os.environ.get('TOKEN')
 bot = telebot.TeleBot(TOKEN)
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_SHIM', None)
+browser = webdriver.Chrome(executable_path="chromedriver", chrome_options=chrome_options)
 
 @bot.message_handler(commands=['start', 'go'])
 def start_handler(message):
